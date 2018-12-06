41398f144df901be65a33193c32a0875428080a5
Teijien <43975613+Teijien@users.noreply.github.com>
2018-12-05 16:18:15 -0800

    ai started

---
+++ OSRS_GE_AI.py
@@ -0,0 +1,14 @@
+import urllib.request, json
+from get_item import get_catalogue, get_graph
+
+#class Node:
+#    def __init__(self):
+
+
+def ID3(x, y, label, node):
+    node = Node()
+
+if __name__ == "__main__":
+    id = int(input("Enter item ID"))
+    graph = get_graph(id)
+    print(graph['daily'])
