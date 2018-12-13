7776313f9d9c44067cc8005afe33e51cab6e602c
themonkeytoucher <mysterysteel@gmail.com>
2018-12-13 14:14:37 -0800

    something with osrs_ge_ai

--- OSRS_GE_AI.py
+++ OSRS_GE_AI.py
@@ -9,11 +9,19 @@ import math
 def ID3(x, y, label, node):
     node = Node()
 
-
+"""
+Input: List of values based on each training set
+Output: An entropy value
+"""
 def entropy(pos, neg):
 	sumpos = sum(pos)
 	sumneg = sum(neg)
 	return sumpos*log(sumpos, 2) - sumneg*log(sumneg, 2)
+
+"""
+
+"""
+def information_gain(pos, neg)
 
 if __name__ == "__main__":
     id = int(input("Enter item ID"))
